f69b61a0116b2e4e7b7eeef91a178830e5c523b1
Aurelie Raout <aurelieraout@gmail.com>
2025-02-14 13:12:48 +0100

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :movies, only: [ :index ]
+  resources :movies, only: [ :index ] do
+    resources :bookmarks, only: [ :index, :show, :create, :new, :destroy ]
+  end
   resources :lists, only: [ :index, :show, :create, :new, :destroy ]
 end
